f1f19d74403f30aa778ba6d9e8a58550827643be
Cage37 <casiraghi.smn@gmail.com>
2021-06-09 14:05:45 +0200

    Milestone-3 complete

--- milestone-3/assets/js/main.js
+++ milestone-3/assets/js/main.js
@@ -1,6 +1,6 @@
-// Milestone 2:
-// Trasformiamo la stringa statica della lingua in una vera e propria bandiera della nazione corrispondente, gestendo il caso in cui non abbiamo la bandiera della nazione ritornata dall’API (le flag non ci sono in FontAwesome).
-// Allarghiamo poi la ricerca anche alle serie tv. Con la stessa azione di ricerca dovremo prendere sia i film che corrispondono alla query, sia le serie tv, stando attenti ad avere alla fine dei valori simili (le serie e i film hanno campi nel JSON di risposta diversi, simili ma non sempre identici)
+// Milestone 3: In questa milestone come prima cosa aggiungiamo la copertina del film o della serie al nostro elenco. 
+// Ci viene passata dall’API solo la parte finale dell’URL, questo perché poi potremo generare da quella porzione di URL tante dimensioni diverse.
+// Dovremo prendere quindi l’URL base delle immagini di TMDB:https://image.tmdb.org/t/p/​ per poi aggiungere la dimensione che vogliamo generare(troviamo tutte le dimensioni possibili a questo link:https://www.themoviedb.org/talk/53c11d4ec3a3684cf4006400​) per poi aggiungere laparte finale dell’URL passata dall’API.Esempio di URL:https://image.tmdb.org/t/p/w342/wwemzKWzjKYJFfCeiB57q3r4Bcm.png
 
 const app = new Vue ({
 
@@ -13,6 +13,7 @@ const app = new Vue ({
         query: "",
         movies: [],
         series: [],
+        error: null,
     },
 
     methods: {
@@ -36,6 +37,9 @@ const app = new Vue ({
                         this.movies[index].original_language = "US" 
                     }
                 }
+            }).catch(e =>{
+                console.error(e);
+                this.error = "Sorry, something went wrong! " + e;
             })
 
             let completeURLS = `${this.urlS}?api_key=${this.apiKey}&query=${search}`
@@ -53,6 +57,9 @@ const app = new Vue ({
                         this.series[index].original_language = "US" 
                     } 
                 }
+            }).catch(e =>{
+                console.error(e);
+                this.error = "Sorry, something went wrong! " + e;
             })
         },
     },
